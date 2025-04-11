026bb8c81a4fd47762595f55f86b0111c15da653
trizo1104 <vominhtri221104@gmail.com>
2025-04-12 04:24:25 +0700

    done

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -28,9 +28,13 @@ app.use("/api/auth", authRoute);
 app.use("/api/message", messageRoute);
 
 if (process.env.NODE_ENV === "production") {
+  const frontendPath = path.resolve(__dirname, "../../Frontend/dist");
+
+  app.use(express.static(frontendPath));
   app.use(express.static(path.join(__dirname, "../../Frontend")));
 
   app.get("*", (req, res) => {
+    res.sendFile(path.join(frontendPath, "index.html"));
     res.sendFile(path.join(__dirname, "../../Frontend", "dist", "index.html"));
   });
 }
