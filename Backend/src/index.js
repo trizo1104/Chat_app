9eac6c3e8e37569f93c556f96140c584f6a2a33a
trizo1104 <133557734+trizo1104@users.noreply.github.com>
2025-04-12 04:10:34 +0700

    Update index.js

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -28,10 +28,10 @@ app.use("/api/auth", authRoute);
 app.use("/api/message", messageRoute);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../Frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../../Frontend")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../Frontend", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../../Frontend", "dist", "index.html"));
   });
 }
 
